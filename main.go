9b40d16db34eae90ab99042810ce86c1fb12abcc
ele <s.ivanischenko@gmail.com>
2017-07-12 21:39:43 +0300

    qwe

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	app.AttachView(view.HTML("./views", ".html").Layout("layout.html").Reload(true))
 	app.Get("/gallery/{id}", func(ctx context.Context) {
 		Res := Result{}
-		apiUrl := "http://localhost:8080"
+		apiUrl := "http://137.74.159.28:8080"
 		_, err := resty.R().
 			SetQueryString("type=Gallery&id=" + ctx.Params().Get("id")).
 			SetResult(&Res).Get(apiUrl+"/api/content")
